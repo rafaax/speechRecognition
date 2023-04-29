757dc05a7884c4df145703dfca41d3afe6f208b9
raphael <rafavfx1@gmail.com>
2023-04-29 15:58:41 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+    <title>Ferias</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    
+</head>
+<body>
+    <header>
+        <img src="https://media.discordapp.net/attachments/945129584934944839/1036776029093630022/unknown.png?width=1200&height=252">
+        <ul>
+            <li>SEJA BEM VINDO ALUNO DO SSECTOR7<?php ?><li>
+        </ul>
+    </header>
+    <div class="form">
+    <div class="form-name">
+        <form method="post" action="form.php">
+        <h2>QUAL SEU NOME???<h2>
+        <input type="text" name="nome">
+        <h2>QUAL A SUA TURMA???</h2>
+        <select name="turma">
+            <option value="a">Turma A</option>
+            <option value="b">Turma B</option>
+            <option value="c">Turma C</option>
+            <option value="d">Turma D</option>
+            <option value="e">Turma E</option>
+        </select>
+        </div>
+        <div class="form-text">
+        <h1>CONTE SUAS FÉRIAS!!!</h1>
+        <br> 
+        <textarea name="texto" id="convert_text" class="text-input"></textarea>
+        <br>
+        <br>
+        <input type="submit" value="ENVIAR">
+        </form>
+        <input type="submit" id="click_to_record" value="VOZ">
+    </div>
+    </div>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+</html>
